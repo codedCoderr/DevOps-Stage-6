b6311bdfe48f781e7e6ec1e98f217138b348d705
codedcoder <codedcoderrr@gmail.com>
2025-12-08 11:07:27 +0100

    test drift detection

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -24,7 +24,7 @@ variable "ssh_private_key" {
 # 1. Provision the Cloud Server (e.g., an EC2 instance)
 resource "aws_instance" "todo_server" {
   ami            = "ami-0f5fcdfbd140e4ab7" # Find a suitable Ubuntu/Amazon Linux 2 AMI
-  instance_type  = "c7i-flex.large"
+  instance_type  = "t3-small"
   key_name       = "access"
 
   subnet_id = "subnet-0517b2602f8db9eca"
@@ -35,10 +35,6 @@ resource "aws_instance" "todo_server" {
     volume_size = 50 # Increase to 20GB
     volume_type = "gp3"
     delete_on_termination = true
-  }
-  
-  tags = {
-    temp_drift_test = "true"  # Add this line
   }
 
   # 4. Remote commands (e.g., waiting for cloud-init)
